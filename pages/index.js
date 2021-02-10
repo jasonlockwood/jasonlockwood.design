400d7edc0c513b1829c4c1992263d303a21ab291
Jason Lockwood <hello@jasonlockwood.design>
2021-02-09 23:35:28 -0500

    create button component
    - added button.js
    - styled button primary & secondary

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout, { siteTitle } from '../components/layout'
 import Intro from '../components/intro'
+import Button from '../components/button'
+import Link from "next/link";
 
 
 export default function Home() {
@@ -7,6 +9,14 @@ export default function Home() {
     <Layout home>
 
       <Intro />
+
+      <Link href="/about">
+        <a><Button primary>Find out more</Button></a>
+      </Link>
+
+      <Link href="/">
+        <a><Button secondary>Get in touch</Button></a>
+      </Link>
 
     </Layout>
   )
