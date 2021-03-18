25232d27ee582e2a3a130cfb8bd98d56a763ce7e
Jason Lockwood <hello@jasonlockwood.design>
2021-03-18 19:02:20 -0400

    updates to brand project page
    - created gridded sections for content
    - added some assets
    - updated some copy to brand project

--- pages/informedcobrand.js
+++ pages/informedcobrand.js
@@ -62,16 +62,7 @@ export default function Home() {
       </div>
       <Space extra />
       <Space extra />
-      <img src="/images/projects/brand/logo-light.svg" />
-      <Space tiny />
-      <div className="grid grid-cols-2 gap-4">
-        <img src="/images/projects/brand/logo-dark.svg" />
-        <img src="/images/projects/brand/logo-knockout.gif" />
-      </div>
-
-      <Space extra />
-      <Space extra />
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-none md:grid-cols-2 gap-4">
         <h2 className="col-span-2 md:col-span-1">the challenge</h2>
         <p>
           Formerly as Appeagle, the company experienced rapid growth as online
@@ -89,37 +80,81 @@ export default function Home() {
       </div>
       <Space extra />
 
-      <img src="/images/projects/brand/Before&After.svg" />
-
       <Space extra />
       <Space extra />
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-none md:grid-cols-2 gap-4">
         <h2 className="col-span-2 md:col-span-1">the solution</h2>
         <p>
-          A brand identity that would position Informed as the leading price management service for professional online businesses that want to leverage automation, data analysis and advanced reporting to achieve continued growth for their businesses.
+          A brand identity that would position Informed as the leading price
+          management service for professional online businesses that want to
+          leverage automation, data analysis and advanced reporting to achieve
+          continued growth for their businesses.
         </p>
       </div>
       <Space extra />
-
       <Space extra />
-
-      <img src="/images/projects/brand/Deck.png" />
+      <img src="/images/projects/brand/Before&After.svg" />
       <Space extra />
-      <div className="grid grid-cols-2 gap-4">
+      <Space extra />
+      <div className="grid grid-cols-none md:grid-cols-2 gap-4">
         <h2 className="col-span-2 md:col-span-1">developing the strategy</h2>
         <p>
-        We set out to develop brand strategy and positioning for Informed that highlights its commitment to helping online businesses grow with a data-driven approach. The company ideally services established and performant online businesses, although Informed is marketing to broad segments of business with a path in place to grow businesses into ideal customers. The branding had to speak to all of these segments. We developed a brand personality that is clean and smart, yet engaging and welcoming. This serves to build a sense of trust and professionalism that is universal to all levels of business.
+          We set out to develop brand strategy and positioning for Informed that
+          highlights its commitment to helping online businesses grow with a
+          data-driven approach. The company ideally services established and
+          performant online businesses, although Informed is marketing to broad
+          segments of business with a path in place to grow businesses into
+          ideal customers. The branding had to speak to all of these segments.
+          We developed a brand personality that is clean and smart, yet engaging
+          and welcoming. This serves to build a sense of trust and
+          professionalism that is universal to all levels of business.
         </p>
       </div>
       <Space extra />
-      <img src="/images/projects/brand/Opening.svg" />
+      <Space extra />
+      <img src="/images/projects/brand/Deck.png" />
       <Space tiny />
       <div className="grid grid-cols-2 gap-4">
-        <img src="/images/projects/brand/shirt.svg" />
-        <img src="/images/projects/brand/Website.svg" />
+        <img src="/images/projects/brand/logo-dark.svg" />
+        <img src="/images/projects/brand/whiteboard.png" />
+      </div>
+      <Space extra />
+      <Space extra />
+      <div className="grid grid-cols-none md:grid-cols-2 gap-4">
+        <h2 className="col-span-2 md:col-span-1">the logo</h2>
+        <p>
+          The Informed logo is simple, balanced, and bold which maximizes reach
+          and recognition. The brand symbol is an upward trending bar chart
+          consisting of two bars. The symbol integrates seamlessly with the
+          wordmark but also works simply on it's own. These two bars represent
+          business growth and achievement using Informed. The wordmark is set in
+          Cera Pro Bold, a contemporary geometric sans serif. Subtle letterform
+          alterations were made to make it feel uniquely Informed.
+        </p>
+      </div>
+      <Space extra />
+      <Space extra />
+      <img src="/images/projects/brand/Sketch.png" />
+      <Space tiny />
+      <img src="/images/projects/brand/logo-light.svg" />
+      <Space tiny />
+      <div className="grid grid-cols-2 gap-4">
+        <img src="/images/projects/brand/logo-dark.svg" />
+        <img src="/images/projects/brand/logo-knockout.gif" />
       </div>
 
+      <Space extra />
+      <Space extra />
+      <div className="grid grid-cols-2 gap-4">
+        <img src="/images/projects/brand/symbol.svg" />
+        <img src="/images/projects/brand/shirt.svg" />
+      </div>
       <Space tiny />
+
+      <img src="/images/projects/brand/ad.svg" />
+      <Space extra />
+      <Space extra />
+
       <p>
         The leadership group resolved to expand the company's core offering by
         introducing a suite of new data driven services that would both, help
@@ -139,8 +174,7 @@ export default function Home() {
       <Space extra />
 
       <Space extra />
-    
-     
+
       <div id="headroomContainer">
         <Headroom disableInlineStyles upTolerance={0} downTolerance={1}>
           <Pagination
